12d0affb7b3cd77ad2653abdc0089dc10fb58be6
lfernandez55 <lfernandez@weber.edu>
2020-02-02 12:26:34 -0700

    Done. .....everything working...refactored....wtforms routes at top

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.config.from_pyfile('config.cfg')
 db = SQLAlchemy(app)
 
 
-# Define the Role data-model
+# Model Section
 class Course(db.Model):
     __tablename__ = 'courses'
     id = db.Column(db.Integer(), primary_key=True)
@@ -27,7 +27,6 @@ class Course(db.Model):
         string_object = str(self.id) + "|" + str(self.name) +"|" + students
         return string_object
 
-# Define the UserRoles association table
 class StudentCourses(db.Model):
     __tablename__ = 'student_courses'
     id = db.Column(db.Integer(), primary_key=True)
@@ -58,7 +57,6 @@ class Student(db.Model):
         string_object = str(self.id) + "|" + str(self.name) +"|" + str(self.email) + "|" + str(self.age) + "|" + nick_names + "|" + courses
         return string_object
 
-# Define the UserRoles association table
 class StudentNickName(db.Model):
     __tablename__ = 'student_nick_name'
     id = db.Column(db.Integer(), primary_key=True)
@@ -66,10 +64,10 @@ class StudentNickName(db.Model):
     student_id = db.Column(db.Integer, db.ForeignKey('student.id', ondelete='CASCADE'))
 
     def __str__(self):
-        string_object = nick_name
+        string_object = self.nick_name
         return string_object
 
-#Forms Section
+# Forms Section
 
 class StudentNickNameForm(FlaskForm):
     id = IntegerField('id')
@@ -78,25 +76,25 @@ class StudentNickNameForm(FlaskForm):
         # No need for csrf token in this child form
         csrf = False
 
+#these are custom validations that are called in the StudentForm
 def email_at_check(form, field):
     if '@' not in field.data:
         raise ValidationError('Email must contain an @')
 def email_unique(form, field):
     stud = Student.query.filter(Student.email == field.data).first()
-    if stud is not None and stud.id is not form.id.data:
-        raise ValidationError('Not a unique email address')
+    if stud is not None:
+        if str(stud.id) != str(form.id.data):
+            raise ValidationError('Not a unique email address')
 
 class StudentForm(FlaskForm):
     id = HiddenField('id')
     name = StringField('name', validators=[InputRequired()])
     email = StringField('email', validators=[InputRequired(), email_at_check, email_unique])
     age = IntegerField('age', validators=[InputRequired()])
-    # student_nick_names = StringField('student_nick_names', validators=[InputRequired()])
     student_nick_names = FieldList(FormField(StudentNickNameForm), label='Nicknames', min_entries=1)
     add_nickname = SubmitField(label='Add Nicknames')
     remove_nickname = SubmitField(label='Remove Last Nickname Entry')
-    courses = SelectMultipleField('Enrollments', choices=[('1', '1'), ('2', '2')])
-    # country_select_field = wtforms.SelectField(label="Country", coerce=int)
+    courses = SelectMultipleField('Enrollments', choices=[]) #we populate choices later
 
     submit = SubmitField()
 
@@ -110,46 +108,10 @@ class StudentEnrollmentForm(FlaskForm):
     submit = SubmitField()
 
 
-# https://stackoverflow.com/questions/49066046/append-entry-to-fieldlist-with-flask-wtforms-using-ajax
-class ChildForm(FlaskForm):
-
-    name = StringField(label='Name child')
-    age = IntegerField(label='Age child')
-
-    class Meta:
-        # No need for csrf token in this child form
-        csrf = False
-
-class ParentForm(FlaskForm):
-
-    name = StringField(label='Name parent')
-    children = FieldList(FormField(ChildForm), label='Children')
-    add_child = SubmitField(label='Add child')
-
-    submit = SubmitField()
-
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=4000, debug=True)
 
-@app.route('/')
-def home_page():
-    return render_template('index.html')
-
-@app.route('/create_all')
-def create_all():
-    db.create_all()
-    message = "DB Created! (A SQLite DB File Should Appear In Your Project Folder.  " \
-              "Also, if changes are made to the model, running this again should " \
-              "add these changes to the db.)"
-    return render_template('index.html', message=message)
-
-@app.route('/drop_all')
-def drop_all():
-    db.drop_all()
-    message = "DB Dropped!!)"
-    return render_template('index.html', message=message)
-
+# Routes section  (all routes that use WTForms are at top of this section)
 @app.route('/add_students', methods={'GET','POST'})
 def add_students():
     form = StudentForm()
@@ -161,7 +123,8 @@ def add_students():
         studObj = Student(name=form.name.data, email=form.email.data, age=form.age.data)
         db.session.add(studObj)
         db.session.commit()
-        return 'Form Successfully Submitted!'
+        flash('Student Added!!')
+        return redirect(url_for('home_page'))
     return render_template('addStudent.html', form=form)
 
 @app.route('/add_students_with_nn_option', methods={'GET','POST'})
@@ -184,31 +147,6 @@ def add_students_with_nn_option():
         flash('Student Added!!')
         return redirect(url_for('home_page'))
     return render_template('add_stud_w_nn.html', form=form)
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    form = ParentForm()
-
-    if form.add_child.data:
-        form.children.append_entry()
-        return render_template('register.html', form=form)
-
-    if form.validate_on_submit():
-        return "Yabba dabba do!!!!"
-
-    return render_template('register.html', form=form)
-
-@app.route('/add_nicknames_to_student')
-def add_nicknames_to_student():
-    joe = Student.query.filter(Student.email == 'joe@weber.edu').first()
-    nickname_1 = StudentNickName(nick_name="Jojo")
-    nickname_2 = StudentNickName(nick_name="Joey")
-    joe.student_nick_names.append(nickname_1)
-    joe.student_nick_names.append(nickname_2)
-    db.session.add(joe)
-    db.session.commit()
-    message = "Two nicknames added to Joe"
-    return render_template('index.html', message=message)
 
 @app.route('/update_student', methods=['GET', 'POST'])
 def update_student():
@@ -233,42 +171,6 @@ def update_student():
         flash('Student Updated!!')
         return redirect(url_for('home_page'))
     return render_template('update_student.html', form=form)
-
-@app.route('/select_student')
-def select_student():
-    joe = Student.query.filter(Student.email == 'joe@weber.edu').first()
-    message = "Query Results:<br> " + str(joe)
-    return render_template('index.html', message=message)
-
-@app.route('/select_students')
-def select_students():
-    students = Student.query.all()
-    query_results = ""
-    for stud in students:
-        query_results = query_results + str(stud) + "<br>"
-
-    message = "Query Results: <br>" + query_results
-    return render_template('index.html', message=message)
-
-@app.route('/delete_student')
-def delete_student():
-    joe = Student.query.filter(Student.email == 'joe@weber.edu').first()
-    db.session.delete(joe)
-    db.session.commit()
-    message = "Joe deleted from DB"
-    return render_template('index.html', message=message)
-
-@app.route('/add_courses')
-def add_courses():
-    course1 = Course(name="Anthro 1000")
-    db.session.add(course1)
-    course2 = Course(name="English 1100")
-    db.session.add(course2)
-
-    db.session.commit()
-    message = "Two courses added to DB"
-    return render_template('index.html', message=message)
-
 
 @app.route('/enroll_students', methods=['GET', 'POST'])
 def enroll_students():
@@ -301,12 +203,44 @@ def enroll_students():
         return redirect(url_for('home_page'))
     return render_template('enroll_students.html', form=form)
 
-@app.route('/show_course_enrollments')
-def show_course_enrollments():
-    anthro = Course.query.filter(Course.name == 'Anthro 1000').first()
-    english = Course.query.filter(Course.name == 'English 1100').first()
+@app.route('/')
+def home_page():
+    return render_template('index.html')
 
-    message = "Course Enrollments:<br>" + str(anthro) + "<br>" + str(english)
+@app.route('/create_all')
+def create_all():
+    db.create_all()
+    message = "DB Created! (A SQLite DB File Should Appear In Your Project Folder.  " \
+              "Also, if changes are made to the model, running this again should " \
+              "add these changes to the db.)"
+    return render_template('index.html', message=message)
+
+@app.route('/drop_all')
+def drop_all():
+    db.drop_all()
+    message = "DB Dropped!!)"
+    return render_template('index.html', message=message)
+
+
+@app.route('/select_students')
+def select_students():
+    students = Student.query.all()
+    query_results = ""
+    for stud in students:
+        query_results = query_results + str(stud) + "<br>"
+
+    message = "Query Results: <br>" + query_results
+    return render_template('index.html', message=message)
+
+@app.route('/add_courses')
+def add_courses():
+    course1 = Course(name="Anthro 1000")
+    db.session.add(course1)
+    course2 = Course(name="English 1100")
+    db.session.add(course2)
+
+    db.session.commit()
+    message = "Two courses added to DB"
     return render_template('index.html', message=message)
 
 @app.route('/show_student_enrollments')
